b895bc77e2178bca6e14b8616333bd65a36a31df
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 23:08:23 +0530

    Add encapsulation examples: protected and private variables in Python

---
+++ OOPS/encapsulation.py
@@ -0,0 +1,27 @@
+'''PROTECTED VARIABLES'''
+# class Factory:
+#     _a = "Mumbai"     # This is a protected variable
+
+#     def _show(self):
+#         print("A Mumbai factory")
+
+# class Bhopal(Factory):
+#     def show2(self):
+#         print(super()._a)
+
+
+# obj = Bhopal()
+# obj.show2()
+
+
+
+'''PRIVATE VARIABLES'''
+# class Factory:
+#     __a = "Mumbai"     # This is a private variable
+
+#     def show(self):
+#         print(Factory.__a)
+
+
+# obj = Factory()
+# obj.show()
