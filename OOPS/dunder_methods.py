b9fc138978db9b1006e63325f7427b0c008a8681
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 23:27:42 +0530

    feat: add dunder methods like __str__ and __add__

---
+++ OOPS/dunder_methods.py
@@ -0,0 +1,21 @@
+# class Animal:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#         return f"Hello, your name and age are {self.name} and {self.age} respectively."
+    
+#     def __add__(self, other):
+#         sum = 0
+#         for i in other:
+#             sum += i.age
+
+#         return f"Sum of ages are {self.age + sum}"
+
+
+# obj = Animal("Tanishq", 20)
+# obj2 = Animal("dolphin",14)
+# obj3 = Animal("tiger",34)
+
+# print(obj + (obj2, obj3))
