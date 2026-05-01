e447d1a2798a6a74f9a32bf8a0fef74be8c86cd2
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 22:56:48 +0530

    Add polymorphism examples: method overriding and duck typing

---
+++ OOPS/polymorphism.py
@@ -0,0 +1,29 @@
+# class Animal:
+#     def show(self):
+#         print("Hello, I am Tanishq")
+
+# class Dog(Animal):
+#     def show(self):
+#         print("Hello, I am a Human")
+
+
+# obj2 = Dog()
+# obj2.show()
+
+
+
+'''DUCK TYPING'''
+# class Animal:
+#     def show(self):
+#         print("I am showing")
+
+# class Dog():
+#     def show(self):
+#         print("Hello, I am a Human")
+
+
+# obj = Animal()
+# obj2 = Dog()
+
+# obj.show()
+# obj2.show()
