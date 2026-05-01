bbc3a4846875abf360c443e0399cf6b82698c13a
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 16:08:25 +0530

    feat: add tuple operations

---
+++ Data_Structures/tuple.py
@@ -0,0 +1,36 @@
+'''
+Core Properties of Tuples:
+1. Tuples are immutable (cannot be modified after creation).
+2. Tuples are ordered (maintain insertion order).
+3. Tuples support indexing and slicing.
+4. Tuples are dynamic (can grow or shrink in size).
+5. Tuples can store heterogeneous data types.
+6. Tuples allow duplicate elements.
+'''
+
+a = (1, 2, 3, 4, 5, 5, 5, 6.7, False, "hello", print)
+
+# print(a)
+# print(a[1])
+# print(a[-1])
+
+
+# for i in a:
+#     print(i, end=" ")
+
+
+# print(dir(tuple))    # prints all the methods and attributes of the tuple class
+
+
+
+'''Tuple Functions'''
+# index = a.index(5)
+# print(index)
+
+# count = a.count(5)
+# print(count)
+
+
+'''Tuple Unpacking'''
+# a,b,c,d = (1,2,3,4)
+# print(a,b,c,d)
