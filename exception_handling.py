f7d1e24d7d144a8a261e76a55ba5e6ccd8ccda0f
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 20:37:37 +0530

    feat: add exception handling examples

---
+++ exception_handling.py
@@ -0,0 +1,26 @@
+'''TRY, EXCEPT, ELSE and FINALLY'''
+# a = int(input("Enter a number: "))
+
+# try:
+#     print(10/a)
+# except Exception as error:
+#     print(f"There is an error as {error}")
+# else:
+#     print("This is executed when there is no error")
+# finally:
+#     print("This is always executed")
+
+
+
+'''Raise Keyword'''
+# age = int(input("Enter your age: "))
+
+# try:
+#     if age < 10 or age > 18:
+#         raise ValueError("Age must be between 10 and 18.")
+#     else:
+#         print("You are eligible to vote.")
+# except Exception as err:
+#     print(f"Error: {err}")
+
+# print("Hello World")
