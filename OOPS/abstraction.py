71f5ce14ff648526ffb6b5f4a4279ccbc0ec3c07
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-05-01 23:18:28 +0530

    feat: add abstraction using ABC

---
+++ OOPS/abstraction.py
@@ -0,0 +1,39 @@
+# from abc import ABC, abstractmethod
+
+
+# class abstract(ABC):
+#     @abstractmethod
+#     def perimeter(self):
+#         pass
+
+#     @abstractmethod
+#     def area(self):
+#         pass
+
+
+
+# class Square(abstract):
+#     def __init__(self, side):
+#         self.side = side
+
+#     def perimeter(self):
+#         print("I have created")
+
+#     def area(self):
+#         print("I have created this also")
+
+
+# class Circle(abstract):
+#     def __init__(self, radius):
+#         self.radius = radius
+
+#     def perimeter(self):
+#         print("I have created")
+
+#     def area(self):
+#         print("I have created this also")
+
+
+
+# obj = Circle(56)
+# obj2 = Square(23)
